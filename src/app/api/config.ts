0e89d342ce3227d589f6b54d3daa1a816da310c1
Shin Thant <shinthant10684@gmail.com>
2023-06-29 22:00:54 +0630

    change backend url

--- src/app/api/config.ts
+++ src/app/api/config.ts
@@ -1,5 +1,5 @@
 const URL =
-	process.env.VITE_ENV === "production"
+	import.meta.env.VITE_ENV === "production"
 		? "https://twitter-api-hj4f.onrender.com/api/v1"
 		: "http://localhost:3500/api/v1";
 export default URL;
