962e71a9cfa0478b084288bed74fdcb61aeb1d37
Shin-Thant <shinthant10684@gmail.com>
2023-09-08 01:46:37 +0630

    update tweet schema

--- src/schemas/TweetSchema.ts
+++ src/schemas/TweetSchema.ts
@@ -6,7 +6,11 @@ const MAX_LIMIT = 120;
 const MAX_LIMIT_ERROR = "Caption cannot contain more thant 120 characters!";
 
 export const TweetCreateSchema = z.object({
-	content: z.string({ invalid_type_error: INVALID_TYPE_ERROR }).optional(),
+	content: z
+		.string({ invalid_type_error: INVALID_TYPE_ERROR })
+		.trim()
+		.max(MAX_LIMIT, MAX_LIMIT_ERROR)
+		.optional(),
 });
 export type TweetCreateInput = z.infer<typeof TweetCreateSchema>;
 
