dce6f9c62720cf3466135d4d81a3cb157ba92c0c
Shin-Thant <shinthant10684@gmail.com>
2023-10-06 00:27:23 +0630

    show tweet info in tweet detail page

--- src/pages/TweetDetails.tsx
+++ src/pages/TweetDetails.tsx
@@ -1,10 +1,19 @@
-import { Box, Container } from "@mui/material";
+import { Box, Divider } from "@mui/material";
+import { Helmet } from "react-helmet-async";
 import { useParams } from "react-router-dom";
-import { useGetTweetByIdQuery } from "../features/tweet/tweetApiSlice";
+import BackButton from "../components/buttons/BackButton";
+import TweetActions from "../components/tweet/TweetActions";
+import TweetBody from "../components/tweet/TweetBody";
 import TweetHeader from "../components/tweet/TweetHeader";
+import TweetCommentBtn from "../components/tweet/buttons/TweetCommentBtn";
+import TweetLikeBtn from "../components/tweet/buttons/TweetLikeBtn";
+import TweetShareBtn from "../components/tweet/buttons/TweetShareBtn";
+import Container from "../containers/Container";
+import { useGetTweetByIdQuery } from "../features/tweet/tweetApiSlice";
 
 export default function TweetDetails() {
 	const { id: tweetId } = useParams();
+
 	const { isFetching, data } = useGetTweetByIdQuery(
 		{ tweetId: tweetId ?? "" },
 		{
@@ -18,30 +27,69 @@ export default function TweetDetails() {
 	}
 
 	return (
-		<Box>
-			<Container
-				sx={{
-					maxWidth: { xs: "xs", normal_sm: "sm", md: "88%" },
-					pt: 3,
-					px: { xs: 0, sm: 3 },
-					"&.MuiBox-root": {
-						px: 0,
-					},
-					color: "text.primary",
-				}}
-			>
+		<>
+			<Helmet>
+				<title>
+					{data ? `${data.owner.username}'s Tweet` : "Tweet Details"}
+				</title>
+			</Helmet>
+
+			<Container sx={{ color: "text.primary" }}>
+				<BackButton text={"Tweet"} spacing={2} sx={{ mb: 4, px: 1 }} />
+
 				{isFetching ? (
 					"fetching..."
 				) : !data ? (
 					"no data"
 				) : (
-					<TweetHeader
-						owner={data.owner}
-						tweetId={data._id}
-						createdAt={data.createdAt}
-					/>
+					<>
+						<TweetHeader
+							owner={data.owner}
+							tweetId={data._id}
+							createdAt={data.createdAt}
+						/>
+
+						<Box
+							sx={{
+								my: 0.5,
+								pl: { xs: 5.5, ss: 7, sm: 7.5 },
+							}}
+						>
+							<TweetBody tweet={data} />
+						</Box>
+
+						<Divider />
+
+						<TweetActions
+							likeBtn={
+								<TweetLikeBtn
+									likes={data.likes}
+									tweetId={data._id}
+								/>
+							}
+							commentBtn={
+								<TweetCommentBtn
+									comments={data.comments}
+									tweetId={data._id}
+								/>
+							}
+							shareBtn={
+								<TweetShareBtn
+									shares={data.shares}
+									tweetId={data._id}
+								/>
+							}
+						/>
+
+						<Divider sx={{ mb: 2 }} />
+
+						{/* show comment */}
+						{/* {data.comments.map((comment) => (
+							<div key={comment._id}>{comment.body}</div>
+						))} */}
+					</>
 				)}
 			</Container>
-		</Box>
+		</>
 	);
 }
