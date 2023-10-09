1ac8763ce69db3273ba88d91f8794920f3fb56ed
Shin-Thant <shinthant10684@gmail.com>
2023-10-10 00:22:23 +0630

    create get comments query

--- src/features/comment/commentApiSlice.ts
+++ src/features/comment/commentApiSlice.ts
@@ -1,7 +1,24 @@
 import apiSlice from "../../app/api/apiSlice";
+import { Comment } from "./commentTypes";
 
 const commentApiSlice = apiSlice.injectEndpoints({
 	endpoints: (builder) => ({
+		getComments: builder.query<Comment[], { tweetId: string }>({
+			query: ({ tweetId }) => `/tweets/${tweetId}/comments`,
+			providesTags: (result, _error, { tweetId }) => {
+				if (!result) {
+					return [{ type: "Comments", id: `/${tweetId}/LIST` }];
+				}
+				return [
+					...result.map((comment) => ({
+						type: "Comments" as const,
+						id: `/${tweetId}/${comment._id}`,
+					})),
+					{ type: "Comments", id: `/${tweetId}/LIST` },
+				];
+			},
+		}),
+
 		addComment: builder.mutation<string, { tweetId: string; body: string }>(
 			{
 				query: ({ body, tweetId }) => ({
@@ -19,4 +36,4 @@ const commentApiSlice = apiSlice.injectEndpoints({
 	}),
 });
 
-export const { useAddCommentMutation } = commentApiSlice;
+export const { useGetCommentsQuery, useAddCommentMutation } = commentApiSlice;
