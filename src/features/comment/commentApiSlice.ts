53b067299ba56d9a794bebe20f52d05f5f5eeb1a
Shin-Thant <shinthant10684@gmail.com>
2023-10-10 00:07:50 +0630

    update add comment api slice

--- src/features/comment/commentApiSlice.ts
+++ src/features/comment/commentApiSlice.ts
@@ -4,10 +4,12 @@ const commentApiSlice = apiSlice.injectEndpoints({
 	endpoints: (builder) => ({
 		addComment: builder.mutation<string, { tweetId: string; body: string }>(
 			{
-				query: (arg) => ({
-					url: "/comments",
+				query: ({ body, tweetId }) => ({
+					url: `/tweets/${tweetId}/comments`,
 					method: "POST",
-					body: arg,
+					body: {
+						body,
+					},
 				}),
 				invalidatesTags: (_res, _err, arg) => [
 					{ type: "Tweets", id: arg.tweetId },
