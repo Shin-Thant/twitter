d010d1fe3f07348099eb31d8f49b21b248cae690
Shin-Thant <shinthant10684@gmail.com>
2023-10-11 22:08:17 +0630

    create common button to comment

---
+++ src/components/buttons/CommentButton.tsx
@@ -0,0 +1,44 @@
+import CommentOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
+import CommentFilledIcon from "@mui/icons-material/TextsmsRounded";
+import { useTweetInfoModal } from "../../hooks/useTweetInfoModal";
+import CardButton from "./CardButton";
+
+type Props = {
+	isCommentedByLoginUser: boolean;
+	count: number;
+	openCommentModal: () => void;
+};
+
+const CommentButton = ({
+	isCommentedByLoginUser,
+	count,
+	openCommentModal,
+}: Props) => {
+	const { setIsOpen: setIsInfoModalOpen } = useTweetInfoModal();
+
+	const onComment = () => {
+		if (!isCommentedByLoginUser) {
+			setIsInfoModalOpen(true);
+			return;
+		}
+		openCommentModal();
+	};
+
+	return (
+		<CardButton
+			isLoading={false}
+			label={count}
+			isDoneByLoginUser={isCommentedByLoginUser}
+			type="comment"
+			handleClick={onComment}
+		>
+			{isCommentedByLoginUser ? (
+				<CommentFilledIcon fontSize="small" />
+			) : (
+				<CommentOutlinedIcon fontSize="small" />
+			)}
+		</CardButton>
+	);
+};
+
+export default CommentButton;
