71a3c73fc62d93b56bd92cf9d8a70ba03bb88866
Shin-Thant <shinthant10684@gmail.com>
2023-10-14 23:41:38 +0630

    fix reply button display bug

--- src/components/comments/ReplyButton.tsx
+++ src/components/comments/ReplyButton.tsx
@@ -1,26 +1,33 @@
 import { useAppSelector } from "../../app/hooks";
+import { ListResultComment } from "../../features/comment/commentTypes";
 import { userIdSelector } from "../../features/user/userSlice";
+import { useReplyCreateModal } from "../../hooks/useReplyCreateModal";
 import CommentButton from "../buttons/CommentButton";
 
 type Props = {
 	ownerId: string;
 	commentId: string;
 	tweetId: string;
-	nestedCommentCount: number;
+	replies: ListResultComment[];
 };
 
-const ReplyButton = ({ nestedCommentCount, ownerId }: Props) => {
+const ReplyButton = ({ tweetId, commentId, replies }: Props) => {
 	const loginUserId = useAppSelector(userIdSelector);
 	const isCommentedByLoginUser: boolean =
-		nestedCommentCount > 0 ? loginUserId === ownerId : false;
+		!!replies.length && loginUserId
+			? !!replies.find((reply) => reply.owner._id === loginUserId)
+			: false;
+
+	const openModal = useReplyCreateModal().openModal;
 
 	const openCommentModal = () => {
-		//
+		openModal({ originId: commentId, tweetId });
 	};
 
 	return (
 		<CommentButton
-			count={nestedCommentCount}
+			loginUserId={loginUserId}
+			count={replies.length}
 			isCommentedByLoginUser={isCommentedByLoginUser}
 			openCommentModal={openCommentModal}
 		/>
